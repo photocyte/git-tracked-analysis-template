6857f8179b258416c577ccf82979886d82c2f58a
Timothy R. Fallon <tfallon@ucsd.edu>
2024-11-07 12:54:53 -0800

    reinit

--- re-init.sh
+++ re-init.sh
@@ -1,7 +1,7 @@
 echo "**Recommend renaming the parent folder from 'git-tracked-analysis-template' to something unique and informative!**"
 rm -rf .git
 git init
-git add ./*
-git rm --no-cached re-init.sh
+git add ./* .gitignore
+git reset HEAD ./re-init.sh
 git commit -m "first commit"
 
