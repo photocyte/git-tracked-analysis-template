64e6231a168b45b38c3914e5d25c0fdfe6800b2d
Timothy R. Fallon <tfallon@ucsd.edu>
2024-11-07 12:49:10 -0800

    refresh re-init

--- REINIT.sh
+++
@@ -1,2 +0,0 @@
-rm -rf .git
-rm -f REINIT.sh &
---
+++ re-init.sh
@@ -0,0 +1,5 @@
+rm -rf .git
+git init
+echo "Recommend renaming the parent folder from 'git-tracked-analysis-template' to something unique and informative!"
+rm -f re-init.sh &
+
