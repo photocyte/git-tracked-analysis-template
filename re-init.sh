03339b44f13acb85f20f116d0c7001cf430b04cd
Timothy R. Fallon <tfallon@ucsd.edu>
2024-11-07 13:10:00 -0800

    reinit

--- re-init.sh
+++ re-init.sh
@@ -5,5 +5,14 @@ git add ./* .gitignore
 git reset ./re-init.sh
 git reset ./README.md
 git commit -m "first commit"
+
+echo "#########"
+echo "To sync up a local git directory, with this one (assuming ssh access), do this:"
+echo "git clone ssh://$(whoami)@$(hostname):$(pwd)"
+echo "git config receive.denyCurrentBranch ignore"
+echo "git reset --hard"
+echo "#########"
+
 rm -f ./README.md
 rm -f ./re-init.sh &
+
