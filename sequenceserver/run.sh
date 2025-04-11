3455c7b6e7b46e949f127732916fd908e13e952c
Timothy R. Fallon <tfallon@ucsd.edu>
2025-04-11 13:49:41 -0700

    update sequenceserver stub to include singularity instructions

--- sequenceserver/run.sh
+++ sequenceserver/run.sh
@@ -1,4 +1,11 @@
+## Docker format
 #docker pull wurmlab/sequenceserver                                                                               
 #docker run -v "$(pwd):/db" --rm -it -p 4567:4567 wurmlab/sequenceserver sequenceserver -d /db
 docker run -v "$(pwd):/db" --rm -it -p 4567:4567 wurmlab/sequenceserver sequenceserver -d /db -m
 
+## Singularity format
+#singularity pull docker://wurmlab/sequenceserver
+#mkdir -p ~/containers/
+#mv sequenceserver_latest.sif ~/containers/
+#singularity exec --pwd /sequenceserver --bind "$(pwd)":/db ~/containers/sequenceserver_latest.sif bundle exec sequenceserver -d /db -m
+singularity exec --pwd /sequenceserver --bind "$(pwd)":/db ~/containers/sequenceserver_latest.sif bundle exec sequenceserver -d /db
